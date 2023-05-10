957bbc0bb5f232c1b93468e54afbe65af33d474b
JURI <jr4426@naver.com>
2023-05-10 12:26:54 +0900

    [+] first commit

---
+++ Dmd/src/main/java/ddwu/spring/Dmd/DmdApplication.java
@@ -0,0 +1,13 @@
+package ddwu.spring.Dmd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmdApplication.class, args);
+	}
+
+}
---
+++ Dmd/src/test/java/ddwu/spring/Dmd/DmdApplicationTests.java
@@ -0,0 +1,13 @@
+package ddwu.spring.Dmd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
