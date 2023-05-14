535aba6767490d1cca0b60e69e2a7f8495adefb2
jung <jung@localhost>
2023-05-14 15:33:50 +0900

    jsp실행수

--- Dmd/src/main/java/ddwu/spring/Dmd/DmdController.java
+++ Dmd/src/main/java/ddwu/spring/Dmd/DmdController.java
@@ -1,23 +1,17 @@
 package ddwu.spring.Dmd;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
+@RequestMapping("/indedx")
 public class DmdController {//기본 컨트롤러
 	
-    // http://localhost:8080/ 으로 접속 시
-    @RequestMapping("/")
-    public String welcome() {
-        return "welcome";
-    }
-    
-    // http://localhost:8080/index 로 접속 시
-    @RequestMapping("/index")
-    public String index() {
-    	System.out.println("index controller start");
-        return "index";
+	@GetMapping
+    public String indedx() {
+        return "indedx";
     }    
     
 }
