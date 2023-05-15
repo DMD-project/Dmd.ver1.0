5afc22d5b9d55339fdd870e893cbd38cc1bc345c
jung <jung@localhost>
2023-05-15 18:40:38 +0900

    index category 추가

--- Dmd/src/main/java/ddwu/spring/Dmd/controller/HomeController.java
+++ Dmd/src/main/java/ddwu/spring/Dmd/controller/HomeController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 	
+	
+	// http://localhost:8080/index
 	@RequestMapping(value = "/", method = RequestMethod.GET)
     public String indedx() {
 		
