98afe1375c544ad05980f43f826423596dbf8f95
진고은 <1201silver@naver.com>
2023-05-16 22:11:35 +0900

    [update] jsp 화면 출력

--- Dmd/src/main/java/ddwu/spring/Dmd/controller/HomeController.java
+++ Dmd/src/main/java/ddwu/spring/Dmd/controller/HomeController.java
@@ -7,12 +7,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 	
-	
 	// http://localhost:8080/index
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-    public String indedx() {
+    public String index() {
 		
 		System.out.println("home controller start");
-        return "indedx";
+        return "/index";
     }    
 }
